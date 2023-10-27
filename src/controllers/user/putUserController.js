1ade1669bab28ae5c404d4599219a2e8db3ce4fc
Evelyn Pereira <eve.cari.perey@gmail.com>
2023-10-27 02:20:26 -0300

    CRUD User Completo

---
+++ src/controllers/user/putUserController.js
@@ -0,0 +1,17 @@
+const { User } = require("../../db");
+
+const putUserController = async (userId, updatedData) => {
+  // Verifica si el usuario con el ID proporcionado existe
+  const existingUser = await User.findByPk(userId);
+
+  if (!existingUser) {
+    throw new Error("Usuario no encontrado");
+  }
+
+  // Actualiza los campos proporcionados en updatedData
+  await existingUser.update(updatedData);
+
+  return existingUser;
+};
+
+module.exports = { putUserController };
