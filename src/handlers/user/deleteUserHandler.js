b82326c035c4076fba068a87fe42c3f2dc17777e
Evelyn Pereira <eve.cari.perey@gmail.com>
2023-10-27 02:27:12 -0300

    Crud actualizado

---
+++ src/handlers/user/deleteUserHandler.js
@@ -0,0 +1,17 @@
+const {
+  deleteUserController,
+} = require("../../controllers/user/deleteUserController");
+
+const deleteUserHandler = async (req, res) => {
+  const userId = req.params.id;
+  try {
+    const deletedUser = await deleteUserController(userId);
+    res.status(200).json(deletedUser);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = {
+  deleteUserHandler,
+};
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,11 +1,12 @@
 const { Router } = require("express");
 const { getUserHandler } = require("../handlers/user/getUserHandler");
 const { postUserHandler } = require("../handlers/user/postUserHandler");
-
+const { putUserHandler } = require("../handlers/user/putUserHandler");
+const { deleteUserHandler } = require("../handlers/user/deleteUserHandler");
 const userRouter = Router();
 
 userRouter.get("/", getUserHandler);
-userRouter.delete("/:id");
-userRouter.put("/:id");
+userRouter.delete("/:id", deleteUserHandler);
+userRouter.put("/:id", putUserHandler);
 userRouter.post("/", postUserHandler);
 module.exports = userRouter;
