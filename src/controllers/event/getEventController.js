3db63280ec84add65ea151eeb345c58365518359
Evelyn Pereira <eve.cari.perey@gmail.com>
2023-10-28 12:55:56 -0300

    commit de prueba

--- src/controllers/event/getEventController.js
+++ src/controllers/event/getEventController.js
@@ -1,47 +1,230 @@
-// //Eve
-// const { Event } = require("../../db");
-
-// const getEventController = async (
-//   name,
-//   description,
-//   category,
-//   capacity,
-//   date,
-//   time,
-//   locationName,
-//   adressLocation,
-//   mapLocation,
-//   image,
-//   bannerImage,
-//   planImage,
-//   views
-// ) => {
-//   const filteredDB = await Event.findAll({
-//     where: {
-//       name: name,
-//     },
-//   });
-
-//   if (filteredDB.length === 0) {
-//     const newEvent = await Event.create({
-//       name,
-//       description,
-//       category,
-//       capacity,
-//       date,
-//       time,
-//       locationName,
-//       adressLocation,
-//       mapLocation,
-//       image,
-//       bannerImage,
-//       planImage,
-//       views,
-//     });
-//     return newEvent;
-//   } else
-//     throw new Error(
-//       "Ya existe un evento creado con ese nombre, por favor ingrese un nombre diferente"
-//     );
-// };
-// module.exports = { getEventController };
+//Eve
+const { Event } = require("../../db");
+//Op.gte: Mayor qué o igual a
+const getEventController = async (search, category, date, price) => {
+  if (search && category && date && price) {
+    const searchName = await Event.findAll({ where: { name: search } });
+    if (searchName) {
+      const filteredDB = await Event.findAll({
+        where: {
+          name: search,
+          category: category,
+          date: date,
+          priceMin: price,
+        },
+      });
+      return filteredDB;
+    } else {
+      const filteredDB = await Event.findAll({
+        where: {
+          locationName: search,
+          category: category,
+          date: date,
+          priceMin: price,
+        },
+      });
+      return filteredDB;
+    }
+  }
+  if (search && category && date) {
+    const searchName = await Event.findAll({ where: { name: search } });
+    if (searchName) {
+      const filteredDB = await Event.findAll({
+        where: {
+          name: search,
+          category: category,
+          date: date,
+        },
+      });
+      return filteredDB;
+    } else {
+      const filteredDB = await Event.findAll({
+        where: {
+          locationName: search,
+          category: category,
+          date: date,
+        },
+      });
+      return filteredDB;
+    }
+  }
+  if (search && category && price) {
+    const searchName = await Event.findAll({ where: { name: search } });
+    if (searchName) {
+      const filteredDB = await Event.findAll({
+        where: {
+          name: search,
+          category: category,
+          priceMin: price,
+        },
+      });
+      return filteredDB;
+    } else {
+      const filteredDB = await Event.findAll({
+        where: {
+          locationName: search,
+          category: category,
+          priceMin: price,
+        },
+      });
+      return filteredDB;
+    }
+  }
+  if (search && date && price) {
+    const searchName = await Event.findAll({ where: { name: search } });
+    if (searchName) {
+      const filteredDB = await Event.findAll({
+        where: {
+          name: search,
+          date: date,
+          priceMin: price,
+        },
+      });
+      return filteredDB;
+    } else {
+      const filteredDB = await Event.findAll({
+        where: {
+          locationName: search,
+          date: date,
+          priceMin: price,
+        },
+      });
+      return filteredDB;
+    }
+  }
+  if (category && date && price) {
+    const filteredDB = await Event.findAll({
+      where: {
+        category: category,
+        date: date,
+        priceMin: price,
+      },
+    });
+    return filteredDB;
+  }
+  if (search && date) {
+    const searchName = await Event.findAll({ where: { name: search } });
+    if (searchName) {
+      const filteredDB = await Event.findAll({
+        where: {
+          name: search,
+          date: date,
+        },
+      });
+      return filteredDB;
+    } else {
+      const filteredDB = await Event.findAll({
+        where: {
+          locationName: search,
+          date: date,
+        },
+      });
+      return filteredDB;
+    }
+  }
+  if (search && price) {
+    const searchName = await Event.findAll({ where: { name: search } });
+    if (searchName) {
+      const filteredDB = await Event.findAll({
+        where: {
+          name: search,
+          priceMin: price,
+        },
+      });
+      return filteredDB;
+    } else {
+      const filteredDB = await Event.findAll({
+        where: {
+          locationName: search,
+          priceMin: price,
+        },
+      });
+      return filteredDB;
+    }
+  }
+  if (search && category) {
+    const searchName = await Event.findAll({ where: { name: search } });
+    if (searchName) {
+      const filteredDB = await Event.findAll({
+        where: {
+          name: search,
+          category: category,
+        },
+      });
+      return filteredDB;
+    } else {
+      const filteredDB = await Event.findAll({
+        where: {
+          locationName: search,
+          category: category,
+        },
+      });
+      return filteredDB;
+    }
+  }
+  if (category && date) {
+    const filteredDB = await Event.findAll({
+      where: {
+        category: category,
+        date: date,
+      },
+    });
+    return filteredDB;
+  }
+  if (category && price) {
+    const filteredDB = await Event.findAll({
+      where: {
+        category: category,
+        priceMin: price,
+      },
+    });
+    return filteredDB;
+  }
+  if (date && price) {
+    const filteredDB = await Event.findAll({
+      where: {
+        date: date,
+        priceMin: price,
+      },
+    });
+    return filteredDB;
+  }
+  if (price) {
+    const filteredDB = await Event.findAll({
+      where: {
+        priceMin: price,
+      },
+    });
+    return filteredDB;
+  }
+  if (date) {
+    const filteredDB = await Event.findAll({
+      where: {
+        date: date,
+      },
+    });
+    return filteredDB;
+  }
+  if (category) {
+    const filteredDB = await Event.findAll({
+      where: {
+        category: category,
+      },
+    });
+    return filteredDB;
+  }
+  if (search) {
+    const searchName = await Event.findAll({ where: { name: search } });
+    if (searchName) {
+      return searchName;
+    } else {
+      const filteredDB = await Event.findAll({
+        where: {
+          locationName: search,
+        },
+      });
+      return filteredDB;
+    }
+  }
+};
+module.exports = { getEventController };
--- src/handlers/event/getEventHandler.js
+++ src/handlers/event/getEventHandler.js
@@ -1,15 +1,16 @@
-// // Eve
-// const {
-//   getEventController,
-// } = require("../../controllers/user/getEventController");
-// const getEventHandler = async (req, res) => {
-//   try {
-//     const user = await getEventController();
-//     res.status(200).json(user);
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// };
-// module.exports = {
-//   getEventHandler,
-// };
+// Eve
+const {
+  getEventController,
+} = require("../../controllers/event/getEventController");
+const getEventHandler = async (req, res) => {
+  const { search, category, date, price } = req.body;
+  try {
+    const event = await getEventController(search, category, date, price);
+    res.status(200).json(event);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+module.exports = {
+  getEventHandler,
+};
