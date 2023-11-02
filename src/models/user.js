268f0a8ee926d447f35522def4eb2409b6cfc46c
Grupo10Henry <148797631+Grupo10Henry@users.noreply.github.com>
2023-11-02 15:17:03 -0300

    Update user.js

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const { DataTypes } = require("sequelize");
+const bcrypt = require('bcryptjs');
 
 module.exports = (sequelize) => {
   sequelize.define(
@@ -49,6 +50,21 @@ module.exports = (sequelize) => {
     },
     {
       paranoid: true,
+      hooks: {
+        beforeCreate: async (user) => {
+          if (user.password) {
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(user.password, salt);
+          }
+        },
+        beforeUpdate: async (user) => {
+          if (user.changed('password')) {
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(user.password, salt);
+          }
+        }
+      }
     }
+
   );
 };
