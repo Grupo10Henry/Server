f231a7d4528feed20273b370257152264ed115fe
Francisco Pertile <panchopertile@hotmail.com>
2023-11-03 10:13:03 -0300

    cambio user, google

--- src/passport-setup.js
+++ src/passport-setup.js
@@ -17,7 +17,7 @@ passport.use(
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         callbackURL: 'http://localhost:3001/auth/google/callback',
-        scope: ['profile', 'email'] // <-- Aquí he agregado el parámetro scope
+        scope: ['profile', 'email'] 
     }, async (token, tokenSecret, profile, done) => {
         let user = await User.findOne({ where: { googleID: profile.id } });
         
@@ -28,9 +28,11 @@ passport.use(
                 name,
                 lastName,
                 email: emails[0].value,
-                password: await bcrypt.hash(googleID, 10), // Usar ID de Google como contraseña por defecto.
-                dob: new Date(), // Poner fecha por defecto o modificar para permitir null.
-                image: photos[0].value
+                password: await bcrypt.hash(googleID, 10), 
+                dob: new Date(), 
+                image: photos[0].value,
+                phone: "",
+                identityCard: ""
             });
         }
         
