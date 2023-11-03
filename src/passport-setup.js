576e1968552130e32e80f4756348fd9d3cfd8d4a
Francisco Pertile <panchopertile@hotmail.com>
2023-11-03 01:21:04 -0300

    server funcionando

--- src/passport-setup.js
+++ src/passport-setup.js
@@ -16,20 +16,24 @@ passport.use(
     new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: '/auth/google/callback'
+        callbackURL: 'http://localhost:3001/auth/google/callback',
+        scope: ['profile', 'email'] // <-- Aquí he agregado el parámetro scope
     }, async (token, tokenSecret, profile, done) => {
         let user = await User.findOne({ where: { googleID: profile.id } });
+        
         if (!user) {
+            const { id: googleID, name: { givenName: name, familyName: lastName }, emails, photos } = profile;
             user = await User.create({
-                googleID: profile.id,
-                name: profile.name.givenName,
-                lastName: profile.name.familyName,
-                email: profile.emails[0].value,
-                password: await bcrypt.hash(profile.id, 10), // Usar ID de Google como contraseña por defecto.
+                googleID,
+                name,
+                lastName,
+                email: emails[0].value,
+                password: await bcrypt.hash(googleID, 10), // Usar ID de Google como contraseña por defecto.
                 dob: new Date(), // Poner fecha por defecto o modificar para permitir null.
-                image: profile.photos[0].value
+                image: photos[0].value
             });
         }
+        
         done(null, user);
     })
 );
