5a4c4e44a99fc1767bc1301c6174da01a4348e5f
Evelyn Pereira <eve.cari.perey@gmail.com>
2023-11-03 09:44:47 -0300

    Arreglando rama

--- src/handlers/user/getUserHandler.js
+++ src/handlers/user/getUserHandler.js
@@ -3,8 +3,9 @@ const {
   getUserController,
 } = require("../../controllers/user/getUserController");
 const getUserHandler = async (req, res) => {
+  const { id } = req.query;
   try {
-    const user = await getUserController();
+    const user = await getUserController(id);
     res.status(200).json(user);
   } catch (error) {
     res.status(500).json({ error: error.message });
