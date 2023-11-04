0386dfac3db946ce70ab0e7cad42856c0c13ab95
Francisco Pertile <panchopertile@hotmail.com>
2023-11-04 15:29:01 -0300

    faq id

--- src/handlers/FAQ/getFAQidHandler.js
+++ src/handlers/FAQ/getFAQidHandler.js
@@ -1,10 +1,10 @@
 const { getFAQById } = require("../../controllers/FAQ/getFAQidController");
 
 const getFAQByIdHandler = async (req, res) => {
-  const { faqID } = req.params;
+  const { id } = req.params;
 
   try {
-    const faq = await getFAQById(faqID);
+    const faq = await getFAQById(id);
     res.status(200).json(faq);
   } catch (error) {
     console.error(error);
