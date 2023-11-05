9ebfc869f4ef1ffa6a402ba3c87e487550c5324e
Francisco Pertile <panchopertile@hotmail.com>
2023-11-05 15:22:54 -0300

    cambio actual

--- src/handlers/loginHandler/loginHandler.js
+++ src/handlers/loginHandler/loginHandler.js
@@ -1,14 +1,42 @@
-const { loginController} = require ("../../controllers/loginController/loginController")
+const { User } = require("../../db");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-const loginHandler = async (req, res) => {
-    const { email, password } = req.body;
+const loginController = async (email, password) => {
+  const user = await User.findOne({ where: { email: email } });
 
-    try {
-        const token = await loginController(email, password);
-        res.json({ token });
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
+  if (!user) {
+    throw new Error("Usuario no encontrado");
+  }
+
+  const validPassword = await bcrypt.compare(password, user.password);
+  if (!validPassword) {
+    throw new Error("Contraseña incorrecta");
+  }
+
+  const userInfo = {
+    userID: user.userID,
+    name: user.name,
+    lastName: user.lastName,
+    email: user.email,
+    password: user.password,
+    phone: user.phone,
+    identityCard: user.identityCard,
+    dob: user.dob,
+    image: user.image, // Asegúrate de que tengas esta propiedad en tu modelo User
+  };
+
+  const token = jwt.sign(
+    { userInfo, isAdmin: user.isAdmin },
+    process.env.JWT_SECRET,
+    { expiresIn: "1h" }
+  );
+
+  return {
+    token,
+    isAdmin: user.isAdmin,
+    userInfo,
+  };
 };
 
-module.exports = { loginHandler };
+module.exports = { loginController };
