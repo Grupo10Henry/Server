f6b1e36dc21562eed839e07ee57ed9425e0fc683
Grupo10Henry <148797631+Grupo10Henry@users.noreply.github.com>
2023-11-08 22:01:08 -0300

    Update postSeatController.js

--- src/controllers/seat/postSeatController.js
+++ src/controllers/seat/postSeatController.js
@@ -7,23 +7,19 @@ async function postSeatController(eventID, rows, columns, sector, price) {
     where: { eventID: eventID, sector: sector },
   });
   if (findSeat.length === 0) {
-    for (let row = 1; row <= rows; row++) {
-      for (
-        let col = "A".charCodeAt(0);
-        col < "A".charCodeAt(0) + columns;
-        col++
-      ) {
-        const seatLocation = `${row}-${String.fromCharCode(col)}`;
+   for (let row = 1; row <= rows; row++) {
+      for (let col = 0; col < columns; col++) {
+        const seatLocation = `${row} - ${String.fromCharCode(65 + col)}`
         seatMatrix.push({
-          eventID: eventID,
-          rows: rows,
-          columns: columns,
-          seatLocation: seatLocation,
-          sector: sector,
-          price: price,
-        });
-      }
-    }
+                  eventID: eventID,
+                  rows: rows,
+                  columns: columns,
+                  seatLocation: seatLocation,
+                  sector: sector,
+                  price: price,
+                });
+              }
+            }
     const createdSeats = await Seat.bulkCreate(seatMatrix);
     return createdSeats;
   } else {
