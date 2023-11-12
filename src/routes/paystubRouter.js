3f40df574ba5ef695bf94a19c46519d5ffeda4e0
luvargaslomnardi <any.vargas231095@gmail.com>
2023-11-12 14:33:11 -0300

    paystub Register

--- src/routes/paystubRouter.js
+++ src/routes/paystubRouter.js
@@ -5,9 +5,18 @@ const {
 const {
   getPaystubsHandler,
 } = require("../handlers/paystub/getPaystubsHandler");
+const {
+  generateQRCodeHandler,
+} = require("../handlers/paystub/generateQRCodeHandler");
+const {
+  getPaystubByDateHandler,
+} = require("../handlers/paystub/getPaystubByDateHandler");
 
 const paystubRouter = Router();
 
 paystubRouter.get("/", getPaystubsHandler); //Obtener todos los paystubs
 paystubRouter.post("/", postPaystubHandler); // crear paystub
+paystubRouter.get("/qrcode/:paystubID", generateQRCodeHandler); // generar   qr
+paystubRouter.get("/:year/:month", getPaystubByDateHandler);
+
 module.exports = paystubRouter;
