f08f6188f89a61c44a8b3a6e4b289fba9773516e
Grupo10Henry <148797631+Grupo10Henry@users.noreply.github.com>
2023-11-13 19:32:42 -0300

    Delete src/routes/epaycoRouter.js

--- src/routes/epaycoRouter.js
+++
@@ -1,8 +0,0 @@
-// const { Router } = require("express");
-// const epaycoRouter = Router();
-
-// const { postEpaycoHandler } = require("../handlers/epayco/postEpaycoHandler");
-
-// epaycoRouter.post("/confirmation", postEpaycoHandler);
-
-// module.exports = epaycoRouter;
