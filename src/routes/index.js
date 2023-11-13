7334b437c5c84dd1c448a60ff99a8400ef4de5f3
Grupo10Henry <148797631+Grupo10Henry@users.noreply.github.com>
2023-11-13 19:25:50 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,35 +5,21 @@ const loginRouter = require("./loginRouter");
 const eventRouter = require("./eventRouter");
 const seatRouter = require("./seatRouter");
 const faqRouter = require("./faqRouter");
-const paystubRouter = require("./paystubRouter");
 const reviewRouter = require("./reviewRouter");
 const contactFormRouter = require("./contactFormRouter");
-const passport = require("passport");
 const companyInfoRouter = require("./companyInfoRouter");
 
+const mercadoPagoRouter = require("./mercadoPagoRouter");
 const router = Router();
 
 router.use("/user", userRouter);
 router.use("/login", loginRouter);
 router.use("/event", eventRouter);
 router.use("/seat", seatRouter);
-router.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"],
-  })
-);
-router.get(
-  "/auth/google/callback",
-  passport.authenticate("google", { failureRedirect: "/login" }),
-  function (req, res) {
-    res.redirect("/home");
-  }
-);
 router.use("/faq", faqRouter);
-router.use("/paystub", paystubRouter);
 router.use("/review", reviewRouter);
 router.use("/contactForm", contactFormRouter);
 router.use("/companyInfo", companyInfoRouter);
+router.use("/mercadoPago", mercadoPagoRouter);
 
 module.exports = router;
