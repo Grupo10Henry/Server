53426cf27610ca266798f24de335759ea52883f4
Evelyn Pereira <eve.cari.perey@gmail.com>
2023-11-17 15:11:46 -0300

    Listo

--- src/routes/userBlockedRouter.js
+++ src/routes/userBlockedRouter.js
@@ -1,8 +1,17 @@
-const getUserBlockedHandler = require("../handlers/userBlocked/getUserBlockedHandler");
-const postUserBlockedHandler = require("../handlers/userBlocked/postUserBlockedHandler");
-const deleteUserBlockedHandler = require("../handlers/userBlocked/deleteUserBlockedHandler");
+const { Router } = require("express");
+const {
+  getUserBlockedHandler,
+} = require("../handlers/userBlocked/getUserBlockedHandler");
+const {
+  postUserBlockedHandler,
+} = require("../handlers/userBlocked/postUserBlockedHandler");
+const {
+  deleteUserBlockedHandler,
+} = require("../handlers/userBlocked/deleteUserBlockedHandler");
 const userBlockedRouter = Router();
 
 userBlockedRouter.get("/", getUserBlockedHandler);
 userBlockedRouter.delete("/:id", deleteUserBlockedHandler);
 userBlockedRouter.post("/", postUserBlockedHandler);
+
+module.exports = userBlockedRouter;
